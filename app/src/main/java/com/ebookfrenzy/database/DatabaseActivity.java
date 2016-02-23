9df8426d65c8e17736b5005925db5f45b0ac41f4
chrismyan <cyan1@my.westga.edu>
2016-02-22 18:17:49 -0800

    Added Update and Clear Database feature

--- app/src/main/java/com/ebookfrenzy/database/DatabaseActivity.java
+++ app/src/main/java/com/ebookfrenzy/database/DatabaseActivity.java
@@ -5,37 +5,101 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class DatabaseActivity extends AppCompatActivity {
+    TextView idView;
+    EditText productBox;
+    EditText quantityBox;
+    Button btnUpdate;
+    MyDBHandler dbHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_database);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        idView = (TextView) findViewById(R.id.productID);
+        productBox = (EditText) findViewById(R.id.productName);
+        quantityBox =
+                (EditText) findViewById(R.id.productQuantity);
+        btnUpdate = (Button)  findViewById(R.id.button4);
+        btnUpdate.setEnabled(false);
+        dbHandler = new MyDBHandler(this, null, null, 1);
+
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_database, menu);
-        return true;
+    public void newProduct (View view) {
+        if(isValid()) {
+            int quantity =
+                    Integer.parseInt(quantityBox.getText().toString());
+
+            Product product =
+                    new Product(productBox.getText().toString(), quantity);
+
+            dbHandler.addProduct(product);
+            productBox.setText("");
+            quantityBox.setText("");
+        }
     }
 
-    @Override
+    public void updateProducts(View view) {
+        if(isValid()) {
+            int quantity =
+                    Integer.parseInt(quantityBox.getText().toString());
+            Product product = new Product(productBox.getText().toString(), quantity);
+            dbHandler.UpdateProduct(product);            productBox.setText("");
+            quantityBox.setText("");
+            btnUpdate.setEnabled(false);
+        }
+    }
+
+    public void lookupProduct (View view) {
+
+        Product product =
+                dbHandler.findProduct(productBox.getText().toString());
+
+        btnUpdate.setEnabled(false);
+        if (product != null) {
+            idView.setText(String.valueOf(product.getID()));
+
+            quantityBox.setText(String.valueOf(product.getQuantity()));
+            btnUpdate.setEnabled(true);
+        } else {
+            idView.setText("No Match Found");
+        }
+    }
+
+    public void removeProduct (View view) {
+
+        boolean result = dbHandler.deleteProduct(
+                productBox.getText().toString());
+
+        if (result)
+        {
+            idView.setText("Record Deleted");
+            productBox.setText("");
+            quantityBox.setText("");
+        }
+        else
+            idView.setText("No Match Found");
+    }
+
+    public void clearProducts(View view) {
+        MyDBHandler dbHandler = new MyDBHandler(this, null, null, 1);
+        dbHandler.onUpgrade(dbHandler.getWritableDatabase(), 1, 1);
+        this.idView.setText("");
+        this.productBox.setText("");
+        this.quantityBox.setText("");
+    }
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
@@ -48,5 +112,21 @@ public class DatabaseActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private boolean isValid() {
+        boolean valid = false;
+        try {
+           Integer.parseInt(quantityBox.getText().toString());
+            if(productBox.getText().toString().equals("")) {
+                return false;
+            }
+            valid = true;
+        } catch (NumberFormatException ex) {
+            Log.w("NumberFormatException", ex.toString());
+        } catch (Exception ex) {
+            Log.w("Exception", ex.toString());
+        }
+        return valid;
     }
 }
